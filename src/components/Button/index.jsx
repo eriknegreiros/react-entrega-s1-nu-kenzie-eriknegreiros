8a6e416eb77fa52818ffa0413900720bf92fe832
Erik <eriksn07@gmail.com>
2022-09-26 18:39:07 -0300

    feat: add btn

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,16 +1,26 @@
-const Button = ({ listTransactions }) => {
+const Button = ({ listTransaction, setListTransactionFiltered, listTransactionFiltered }) => {
+  
   const enter = () => {
-    const enterFiltered = listTransactions.filter(
+    const enterFiltered = listTransaction.filter(
       (element) => element.type === "Entrada"
     );
-    return enterFiltered;
+    setListTransactionFiltered(enterFiltered) ;
+
   };
 
   const expense = () => {
-    const expenseFiltered = listTransactions.filter(
+    const expenseFiltered = listTransaction.filter(
       (element) => element.type === "Despesa"
     );
-    return expenseFiltered;
+    setListTransactionFiltered(expenseFiltered)  ;
+  };
+
+  const all = () => {
+    const enterFiltered = listTransaction.filter(
+      (element) => element.type === "Entrada" || element.type === "Despesa"
+    );
+    setListTransactionFiltered(enterFiltered) ;
+    
   };
 
   return (
@@ -18,7 +28,7 @@ const Button = ({ listTransactions }) => {
       <div className="div_finances">
         <h1 className="text_resume_finances">Resumo Financeiro</h1>
         <div className="div_btn_finances">
-          <button className="btn_finances">Todos</button>
+          <button onClick={all} className="btn_finances">Todos</button>
           <button onClick={enter} className="btn_finances">
             Entradas
           </button>
